876dfd857b13f0f7c39736410621df6118fd2749
AminIsaev <amin.isaev@gmail.com>
2018-05-01 10:32:51 +0700

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-
-app.listen(8080);
-setInterval(() => {
-http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 300000);
